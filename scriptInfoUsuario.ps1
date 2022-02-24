27031e7e18bd78d199920fa049d98991aecee530
naate20 <59051270+naate20@users.noreply.github.com>
2022-02-24 18:59:24 +0100

    Primer Script para obtener informacion de un usuario AD

---
+++ scriptInfoUsuario.ps1
@@ -0,0 +1,41 @@
+<#$nombre = Read-Host "¿Como te llamas?"
+
+Write-Host $nombre.GetType().Name#>
+
+<#
+Get-ADUser -Filter * | Ft SamAccountName, SID, Enabled  Ft -> Format-Table
+
+Información sobre el grupo: (GET-ADUSER –Identity pmovil02 –Properties MemberOf | Select-Object MemberOf).MemberOf
+
+Get-ADGroup -Identity $nombre | Ft 
+
+Get-ADUser -Filter {mail -eq $correo}
+
+#>
+
+Param (
+    [string] $nombre
+)
+
+$userexists = Get-ADUser -Filter {SamAccountName -eq $nombre}
+
+$grupo = Get-ADPrincipalGroupMembership $nombre
+
+if ($userexists) {
+    
+    Write-Output "--- INFORMACION DEL USUARIO ---"
+    Write-Output (Get-ADUser -Identity $nombre | Ft SamAccountName, SID, Enabled)
+
+    if($grupo){
+
+        Write-Output "--- GRUPOS A LOS QUE PERTENECE ---"
+        Write-Output (Get-ADPrincipalGroupMembership $nombre | Select SamAccountName, SID, GroupScope)
+
+    }
+}
+
+else {
+
+    Write-Output "EL USUARIO NO EXISTE" 
+
+}
