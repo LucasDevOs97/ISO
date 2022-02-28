189a5842ea58c3a6bb23555836312bdddbf8bd87
naate20 <natalia420s@gmail.com>
2022-02-28 11:50:05 +0100

    Update scriptLogica.ps1
    Finalización script con operación lógica (primera forma)

--- scriptLogica.ps1
+++ scriptLogica.ps1
@@ -1,67 +1,67 @@
 Param(
-    [Parameter (Mandatory=$true, HelpMessage = "La operacion solo puede ser: xor, nxor",ParameterSetName="user", "u")]
-    [string] $operacion,
-    [Parameter (ParameterSetName="op1")]
-    [bool] $operando1 = $true,
-    [Parameter (ParameterSetName="op2")]
-    [bool] $operando2 = $true,
-    [bool] $resultado
+    [Parameter (Mandatory=$true, HelpMessage = "La operacion solo puede ser: xor, nxor")]
+    [string] [alias("op")] $operacion,
+    [string] [Alias("op2")] $operando1 = "TRUE",
+    [string] [Alias("op1")] $operando2 = "TRUE",
+    [string] $resultado
 )
 
-if ($operacion -eq "xor") {
+$iguales = ($operando1 -eq $operando2)
 
-    if($operando1 -eq $operando2){
+$diferentes = ($operando1 -ne $operando2)
 
-        $operando1 = $true
-        $operando2 = $true
+$output = "$($operando1.ToUpper()) $($operacion.ToUpper()) $($operando2.ToUpper()) = "
+
+if($operacion -eq "xor") {
+
+    if($diferentes){
+
+        $resultado = "TRUE"
+
+        echo "$($output) $($resultado)"
+
+    }
+
+    else {
 
         $resultado = $false
 
-        Write-Output $operando1 $operacion $operando2 $resultado
+        echo echo "$($output) $($resultado)"
 
     }
 
-    else{
-
-        $operando1 = $true
-        $operando2 = $false
-
-        $resultado = $true
-
-        Write-Output $operando1 $operacion $operando2 $resultado
-
-    }
-    
 }
 
-elseif ($operacion -eq "nxor") {
-    
-    if($operando1 -eq $operando2){
+if($operacion -eq "nxor"){
 
-        $operando1 = $true
-        $operando2 = $true
+    if($iguales){
 
-        $resultado = $true
+        $resultado = "TRUE"
 
-        Write-Output $operacion $operando2 $resultado
+        echo echo "$($output) $($resultado)"
 
     }
 
-    else{
+    else {
 
-        $operando1 = $true
-        $operando2 = $false
+        $resultado = "FALSE"
 
-        $resultado = $false
-
-        Write-Output $operando1 $operacion $operando2 $resultado
+        echo echo "$($output) $($resultado)"
 
     }
 
 }
 
 else {
-    
-    Write-Output "-- Error: operacion no reconocida, inténtelo de nuevo --"
+
+    echo "-- Error: operacion no reconocida, inténtelo de nuevo --"
 
 }
+
+<#
+Para que la salida sea en una única línea tenemos que poner todo entre comillas doble y poner el $ antes de los paréntesis para cada variable.
+Para simplificar un poco el código y por posibles modificaciones en el futuro, declaro el output al prnicipio ya que siempre va a ser igual
+pero no podemos poner dentro el parámetro resultado porque no es un valor "fijo", se le da valor después, dentro de las condiciones
+y al declararlo no le damos ningún parámetro por lo que el resultado siempre restaría emn blanco, si le hubiera puesto que por defecto 
+fuese TRUE o FALSE, siempre tendría ese valor y el ejercicio no estaría bien
+#>
