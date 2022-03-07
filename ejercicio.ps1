edd28982b8373cca3c7ad68f9b7ab9bdc5de54e7
naate20 <59051270+naate20@users.noreply.github.com>
2022-03-07 08:43:44 +0100

    ejercicio script activar o desactivar un script

---
+++ ejercicio.ps1
@@ -0,0 +1,14 @@
+# Scripts:
+
+<#- Servicio.ps1 -> Recibe dos parámteros:   
+    - nombre: un nombre de servicio [obligatorio]    
+    - estado: valores activar o desactivar, que aparecen al tabular, si no se recibe, el valor es activar   
+
+- Modificarlo para que compruebe que el servicio recibido como parámetro existe  #>
+
+Param(
+    [Parameter (Mandatory= $true)]
+    [string] $nombre,
+    [array] $estado = ("activar", "desactivar")
+)
+
